0a2c7fb1d32683f7735912c54ca876025959b10a
keer2345 <keer2345@gmail.com>
2024-06-16 00:16:46 +0800

    SecurityFilterChain

--- yudao-framework/yudao-spring-boot-starter-security/src/main/java/com/keer/yudaovue/framework/security/config/YudaoWebSecurityConfigurerAdapter.java
+++ yudao-framework/yudao-spring-boot-starter-security/src/main/java/com/keer/yudaovue/framework/security/config/YudaoWebSecurityConfigurerAdapter.java
@@ -23,7 +23,15 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.List;
+
 /**
+ * 参考：
+ *
+ * <p>https://github.com/DryRun03/JWT3/
+ *
+ * <p>https://blog.csdn.net/qq_55754838/article/details/134620470
+ *
  * @author keer
  * @date 2024-06-02
  */
@@ -36,14 +44,10 @@ public class YudaoWebSecurityConfigurerAdapter {
   @Resource private AuthenticationEntryPoint authenticationEntryPoint;
 
   /** 权限不够处理器 Bean */
-  @Resource
-  private AccessDeniedHandler accessDeniedHandler;
+  @Resource private AccessDeniedHandler accessDeniedHandler;
 
-  /**
-   * Token 认证过滤器 Bean
-   */
-  @Resource
-  private TokenAuthenticationFilter authenticationTokenFilter;
+  /** Token 认证过滤器 Bean */
+  @Resource private TokenAuthenticationFilter authenticationTokenFilter;
 
   /**
    * 由于 Spring Security 创建 AuthenticationManager 对象时，没声明 @Bean 注解，导致无法被注入 通过覆写父类的该方法，添加 @Bean
@@ -55,6 +59,23 @@ public class YudaoWebSecurityConfigurerAdapter {
     return authenticationConfiguration.getAuthenticationManager();
   }
 
+  /***
+   * 配置 URL 的安全配置
+   *
+   * anyRequest          |   匹配所有请求路径
+   * access              |   SpringEl表达式结果为true时可以访问
+   * anonymous           |   匿名可以访问
+   * denyAll             |   用户不能访问
+   * fullyAuthenticated  |   用户完全认证可以访问（非remember-me下自动登录）
+   * hasAnyAuthority     |   如果有参数，参数表示权限，则其中任何一个权限可以访问
+   * hasAnyRole          |   如果有参数，参数表示角色，则其中任何一个角色可以访问
+   * hasAuthority        |   如果有参数，参数表示权限，则其权限可以访问
+   * hasIpAddress        |   如果有参数，参数表示IP地址，如果用户IP和参数匹配，则可以访问
+   * hasRole             |   如果有参数，参数表示角色，则其角色可以访问
+   * permitAll           |   用户可以任意访问
+   * rememberMe          |   允许通过remember-me登录的用户访问
+   * authenticated       |   用户登录后可访问
+   */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     log.info("vsvs...vsvs");
@@ -69,13 +90,16 @@ public class YudaoWebSecurityConfigurerAdapter {
         .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
         // 一堆自定义的 Spring Security 处理器
         .exceptionHandling(
-            c -> c.authenticationEntryPoint(authenticationEntryPoint)
-            .accessDeniedHandler(accessDeniedHandler)
-            );
+            c ->
+                c.authenticationEntryPoint(authenticationEntryPoint)
+                    .accessDeniedHandler(accessDeniedHandler));
 
     // 登录、登录暂时不使用 Spring Security 的拓展点，主要考虑一方面拓展多用户、多种登录方式相对复杂，一方面用户的学习成本较高
 
-    http.authorizeHttpRequests(
+    // 设置每个请求的权限
+    http
+        // ①：全局共享规则
+        .authorizeHttpRequests(
             r ->
                 r
                     // 1.1 静态资源，可匿名访问
@@ -87,14 +111,18 @@ public class YudaoWebSecurityConfigurerAdapter {
                         "/doc.html",
                         "/webjars/**",
                         "/swagger-resources/**",
-                        "/v3/**",
-                        "/admin-api/system/captcha/t2")
+                        "/v3/**"
+                        // "/admin-api/system/captcha/t2"
+                        )
                     .permitAll()
                     .requestMatchers(HttpMethod.POST, "/admin-api/system/captcha/get")
                     .permitAll()
             // .anyRequest()
             // .authenticated()
             )
+        // ②：每个项目的自定义规则
+        // todo
+        // ③：兜底规则，必须认证
         .authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
     // 添加 Token Filter
